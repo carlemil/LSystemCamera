3432bfa3cbcd06a48cfe51eb4d9649439af67074
Carl-Emil Kjellstrand <erbnsman@gmail.com>
2021-02-01 21:52:18 +0100

    Moved analyzer related functions to new ImageAnalyzer class.

---
+++ app/src/main/java/se/kjellstrand/lsystemcamera/ImageAnalyzer.kt
@@ -0,0 +1,124 @@
+package se.kjellstrand.lsystemcamera
+
+import android.annotation.SuppressLint
+import android.graphics.*
+import android.widget.ImageView
+import androidx.camera.core.ImageProxy
+import se.kjellstrand.lsystem.LSystemGenerator
+import se.kjellstrand.lsystem.buildHullFromPolygon
+import se.kjellstrand.lsystem.model.LSTriple
+
+class ImageAnalyzer {
+
+    companion object {
+        private var bitmap: Bitmap? = null
+        private var luminance: Array<FloatArray> = Array(0) { FloatArray(0) }
+        private lateinit var line: MutableList<LSTriple>
+
+        fun updateLSystem(
+            model: LSystemViewModel,
+            imageView: ImageView
+        ) {
+            model.lSystem.value?.let { system ->
+                line = LSystemGenerator.generatePolygon(system, model.getIterations())
+                model.setMaxIterations(system, imageView)
+                line.distinct()
+            }
+        }
+
+        fun analyzeImage(
+            image: ImageProxy,
+            imageView: ImageView,
+            model: LSystemViewModel
+        ): Bitmap? {
+            if (luminance.size != image.width || luminance[0].size != image.height) {
+                luminance = Array(image.height) { FloatArray(image.height) }
+            }
+
+            if (bitmap == null) {
+                bitmap =
+                    Bitmap.createBitmap(imageView.width, imageView.height, Bitmap.Config.ARGB_8888)
+            }
+
+            bitmap?.let { bitmap ->
+                model.lSystem.value?.let { system ->
+                    line = LSystemGenerator.generatePolygon(system, model.getIterations())
+                    line = line.distinct() as MutableList<LSTriple>
+
+                    val scaledLine = updateLSystem(image, imageView, luminance, model, line)
+
+                    val hull = buildHullFromPolygon(scaledLine)
+
+                    val c = Canvas(bitmap)
+                    val bgPaint = Paint()
+                    bgPaint.color = Color.WHITE
+                    c.drawRect(0F, 0F, c.width.toFloat(), c.height.toFloat(), bgPaint)
+                    val paint = Paint()
+                    paint.color = Color.BLACK
+                    paint.style = Paint.Style.FILL_AND_STROKE
+
+                    val polyPath = Path()
+                    //polyPath.fillType = Path.FillType.WINDING
+                    polyPath.moveTo(hull[0].x, hull[0].y)
+                    hull.forEach { p ->
+                        polyPath.lineTo(p.x, p.y)
+                    }
+                    polyPath.close()
+                    c.drawPath(polyPath, paint)
+                }
+
+
+            }
+            image.close()
+            return bitmap
+        }
+
+        @SuppressLint("UnsafeExperimentalUsageError")
+        fun updateLSystem(
+            image: ImageProxy,
+            imageView: ImageView,
+            luminance: Array<FloatArray>,
+            model: LSystemViewModel,
+            line: MutableList<LSTriple>
+        ): MutableList<LSTriple> {
+            val width = imageView.width
+            val plane = image.image?.planes?.get(0)
+
+            // TODO Check if w > h and do opposite
+            val vhDiff = (image.width - image.height) / 2
+            val hRange = 0 until image.height
+            val vRange = vhDiff until image.width - vhDiff
+            for (y in hRange) {
+                for (x in vRange) {
+                    val byte = (plane?.buffer?.get(x + y * plane.rowStride) ?: Byte.MIN_VALUE)
+                    val f = byte.toFloat() / 256f
+                    luminance[image.height - y - 1][x - vhDiff] = 1 - if (f < 0) f + 1 else f
+                }
+            }
+            // TODO END
+
+            model.lSystem.value?.let { system ->
+                val (minWidth, maxWidth) = LSystemGenerator.getRecommendedMinAndMaxWidth(
+                    width, model.getIterations(), system
+                )
+
+                LSystemGenerator.setLineWidthAccordingToImage(
+                    line = line,
+                    luminanceData = luminance,
+                    minWidth = minWidth,
+                    maxWidth = maxWidth
+                )
+
+                val outputSideBuffer = imageView.width / 50
+
+                LSystemGenerator.adjustToOutputRectangle(imageView.width, outputSideBuffer, line)
+
+                line.forEach { p ->
+                    p.x = p.x * imageView.width
+                    p.y = p.y * imageView.height
+                }
+            }
+            return line
+        }
+    }
+}
--- app/src/main/java/se/kjellstrand/lsystemcamera/MainActivity.kt
+++ app/src/main/java/se/kjellstrand/lsystemcamera/MainActivity.kt
@@ -1,9 +1,8 @@
 package se.kjellstrand.lsystemcamera
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.graphics.*
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -15,7 +14,6 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
@@ -24,10 +22,6 @@ import androidx.lifecycle.Observer
 import com.google.android.material.chip.Chip
 import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.android.synthetic.main.activity_main.*
-import se.kjellstrand.lsystem.LSystemGenerator
-import se.kjellstrand.lsystem.LSystemGenerator.generatePolygon
-import se.kjellstrand.lsystem.buildHullFromPolygon
-import se.kjellstrand.lsystem.model.LSTriple
 import se.kjellstrand.lsystem.model.LSystem
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -37,13 +31,11 @@ private const val CAMERA_IMAGE_SIZE = 200
 class MainActivity : AppCompatActivity() {
 
     private lateinit var cameraExecutor: ExecutorService
-    private lateinit var line: MutableList<LSTriple>
 
     private val model: LSystemViewModel by viewModels()
     private val analyzerExecutor = Executors.newSingleThreadExecutor()
     private val bannedSystemNames = listOf("KochSnowFlake")
 
-    private var luminance: Array<FloatArray> = Array(0) { FloatArray(0) }
     private var bitmap: Bitmap? = null
 
     @androidx.camera.core.ExperimentalGetImage
@@ -53,15 +45,17 @@ class MainActivity : AppCompatActivity() {
 
         inflateSystemNameSelectorChips()
 
-        model.lSystem.observe(this, Observer { system ->
-            model.setMaxIterations(system, imageView)
-            line = generatePolygon(system, model.getIterations())
-            line.distinct()
+        model.lSystem.observe(this, Observer {
+            ImageAnalyzer.updateLSystem(model, imageView)
         })
 
         // Set a random system to start with.
-        model.lSystem.value =
-            LSystem.getByName("Moore") // LSystem.systems[Random.nextInt(0, LSystem.systems.size - 1)].name)
+        val defaultSystem = LSystem.getByName("Moore")
+        defaultSystem?.let { system ->
+            model.lSystem.value =
+                system // LSystem.systems[Random.nextInt(0, LSystem.systems.size - 1)].name)
+            ImageAnalyzer.updateLSystem(model, imageView)
+        }
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -73,9 +67,6 @@ class MainActivity : AppCompatActivity() {
         }
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
-
-
-    // TODO move shit to fragment
 
     private fun inflateSystemNameSelectorChips() {
         LSystem.systems
@@ -125,7 +116,10 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         imageAnalysis.setAnalyzer(analyzerExecutor, { image ->
-            analyzeImage(image)
+            bitmap = ImageAnalyzer.analyzeImage(image, imageView, model)
+            runOnUiThread {
+                imageView.setImageBitmap(bitmap)
+            }
         })
 
         try {
@@ -138,93 +132,6 @@ class MainActivity : AppCompatActivity() {
         } catch (exc: Exception) {
             Log.e(TAG, "Use case binding failed", exc)
         }
-    }
-
-    private fun analyzeImage(image: ImageProxy) {
-        if (luminance.size != image.width || luminance[0].size != image.height) {
-            luminance = Array(image.height) { FloatArray(image.height) }
-        }
-
-        if (bitmap == null) {
-            bitmap = Bitmap.createBitmap(imageView.width, imageView.height, Bitmap.Config.ARGB_8888)
-        }
-
-        bitmap?.let { bitmap ->
-            model.lSystem.value?.let { system ->
-                line = generatePolygon(system, model.getIterations())
-                line = line.distinct() as MutableList<LSTriple>
-
-                val scaledLine = updateLSystem(image)
-
-                val hull = buildHullFromPolygon(scaledLine)
-
-                val c = Canvas(bitmap)
-                val bgPaint = Paint()
-                bgPaint.color = Color.WHITE
-                c.drawRect(0F, 0F, c.width.toFloat(), c.height.toFloat(), bgPaint)
-                val paint = Paint()
-                paint.color = Color.BLACK
-                paint.style = Paint.Style.FILL_AND_STROKE
-
-                val polyPath = Path()
-                //polyPath.fillType = Path.FillType.WINDING
-                polyPath.moveTo(hull[0].x, hull[0].y)
-                hull.forEach { p ->
-                    polyPath.lineTo(p.x, p.y)
-                }
-                polyPath.close()
-                c.drawPath(polyPath, paint)
-            }
-
-            runOnUiThread {
-                imageView.setImageBitmap(bitmap)
-            }
-        }
-        image.close()
-    }
-
-    @SuppressLint("UnsafeExperimentalUsageError")
-    private fun updateLSystem(
-        image: ImageProxy,
-    ): MutableList<LSTriple> {
-        val width = imageView.width
-        val plane = image.image?.planes?.get(0)
-
-        // TODO Check if w > h and do opposite
-        val vhDiff = (image.width - image.height) / 2
-        val hRange = 0 until image.height
-        val vRange = vhDiff until image.width - vhDiff
-        for (y in hRange) {
-            for (x in vRange) {
-                val byte = (plane?.buffer?.get(x + y * plane.rowStride) ?: Byte.MIN_VALUE)
-                val f = byte.toFloat() / 256f
-                luminance[image.height - y - 1][x-vhDiff] = 1 - if (f < 0) f + 1 else f
-            }
-        }
-        // en todo
-
-        model.lSystem.value?.let { system ->
-            val (minWidth, maxWidth) = LSystemGenerator.getRecommendedMinAndMaxWidth(
-                width, model.getIterations(), system
-            )
-
-            LSystemGenerator.setLineWidthAccordingToImage(
-                line = line,
-                luminanceData = luminance,
-                minWidth = minWidth,
-                maxWidth = maxWidth
-            )
-
-            val outputSideBuffer = imageView.width / 50
-
-            LSystemGenerator.adjustToOutputRectangle(imageView.width, outputSideBuffer, line)
-
-            line.forEach { p ->
-                p.x = p.x * imageView.width
-                p.y = p.y * imageView.height
-            }
-        }
-        return line
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
